395c63b402970c4e7d0a0923e3799831e92af9b9
WAMaker <wamaker.scorpion@gmail.com>
2019-03-10 21:02:43 +0800

    commit solution

---
+++ Algorithms/142-Linked-List-Cycle-II.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+	let fast = head
+	let slow = head
+	
+	while (fast && fast.next) {
+		fast = fast.next.next
+		slow = slow.next
+		
+		if (fast === slow) {
+			while (head !== fast) {
+				head = head.next
+				fast = fast.next
+			}
+			return head
+		}
+	}
+	
+	return null
+};
+
