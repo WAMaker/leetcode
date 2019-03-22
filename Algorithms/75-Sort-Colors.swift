5cd68ebbff3319a3ee531e669cdc87f8c46dfcfb
WAMaker <wamaker.scorpion@gmail.com>
2019-03-22 23:41:37 +0800

    update solution

--- Algorithms/75-Sort-Colors.swift
+++ Algorithms/75-Sort-Colors.swift
@@ -1,23 +1,29 @@
 class Solution {
     func sortColors(_ nums: inout [Int]) {
-        let count = nums.count
-
-        var h = 1
-        while h < count / 3 {
-            h = 3 * h + 1
+        sort(&nums, 0, nums.count - 1)
+    }
+    
+    func sort(_ nums: inout [Int], _ lo: Int, _ hi: Int) {
+        if lo >= hi {
+            return
         }
-
-        while h >= 1 {
-            for i in h..<count {
-                var j = i
-                while j >= h {
-                    if nums[j] < nums[j - h] {
-                        (nums[j - h], nums[j]) = (nums[j], nums[j - h])
-                    }
-                    j -= h
-                }
+        var lt = lo, gt = hi, v = nums[lo]
+        var i = lo
+        
+        while (i <= gt) {
+            if nums[i] > v {
+                (nums[i], nums[gt]) = (nums[gt], nums[i])
+                gt -= 1
+            } else if nums[i] < v {
+                (nums[i], nums[lt]) = (nums[lt], nums[i])
+                lt += 1
+                i += 1
+            } else {
+                i += 1
             }
-            h /= 3
         }
+        
+        sort(&nums, lo, lt - 1)
+        sort(&nums, gt + 1, hi)
     }
 }
