d30044ab67abbd0666fc291d874bdab7b8e71e79
WAMaker <wamaker.scorpion@gmail.com>
2019-08-15 11:07:36 +0800

    commit solution

---
+++ Algorithms/66-Plus-One.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var digits = digits
+
+        for i in stride(from: digits.count - 1, through: 0, by: -1) {
+            if digits[i] < 9 {
+                digits[i] += 1
+                return digits
+            } else {
+                digits[i] = 0
+            }
+        }
+
+        return [1] + digits
+    }
+}
