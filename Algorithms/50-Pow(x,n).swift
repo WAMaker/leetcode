f28289e700d2e31042fd56bf7bae3ad4c71171f9
WAMaker <wamaker.scorpion@gmail.com>
2019-08-17 10:16:29 +0800

    chore: update solution

--- Algorithms/50-Pow(x,n).swift
+++ Algorithms/50-Pow(x,n).swift
@@ -1,28 +1,17 @@
 class Solution {
 	func myPow(_ x: Double, _ n: Int) -> Double {
-		if x == 0 {
-			return 0
+		var x = n >= 0 ? x : 1 / x
+		var n = abs(n)
+		var ans = 1.0
+		
+		while n > 0 {
+			if n & 1 == 1 {
+				ans *= x
+			}
+			x *= x
+			n >>= 1
 		}
-		let absn = abs(n)
-		var result = calculatePow(x, absn)
-		if n < 0 {
-			result = 1 / result
-		}
-		return result
-	}
-	
-	func calculatePow(_ x: Double, _ n: Int) -> Double {
-		if n == 0 {
-			return 1
-		}
-		if n == 1 {
-			return x
-		}
-		var result = calculatePow(x, n >> 1)
-		result *= result
-		if n & 1 == 1 {
-			result *= x
-		}
-		return result
+		
+		return ans
 	}
 }
