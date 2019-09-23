cf442773adf84212b6ab5f5208dd644a7864de3e
WAMaker <wamaker.scorpion@gmail.com>
2019-09-23 23:58:40 +0800

    commit solution

---
+++ Algorithms/174-dungeon-game.swift
@@ -0,0 +1,33 @@
+class Solution {
+	func calculateMinimumHP(_ dungeon: [[Int]]) -> Int {
+		guard let col = dungeon.first?.count else {
+			return 1
+		}
+		let row = dungeon.count
+		var hps = [[Int]]()
+		for _ in 0 ..< dungeon.count {
+			hps.append([Int](repeating: Int.max, count: col))
+		}
+		
+		for i in stride(from: row - 1, through: 0, by: -1) {
+			for j in stride(from: col - 1, through: 0, by: -1) {
+				if i == row - 1, j == col - 1 {
+					hps[i][j] = max(1, 1 - dungeon[i][j])
+					continue
+				}
+				var hp = Int.max
+				if i < row - 1 {
+					hp = min(hp, hps[i + 1][j] - dungeon[i][j])
+				}
+				if j < col - 1 {
+					hp = min(hp, hps[i][j + 1] - dungeon[i][j])
+				}
+				
+				hps[i][j] = max(1, hp)
+			}
+		}
+		print(hps)
+		
+		return hps[0][0]
+	}
+}
