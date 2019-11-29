b9f74e8fe52d185731635dba146864889fc974ab
WAMaker <wamaker.scorpion@gmail.com>
2019-11-29 23:43:51 +0800

    commit solution

---
+++ Algorithms/371-Sum-of-Two-Integers.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func getSum(_ a: Int, _ b: Int) -> Int {
+        var a = a, b = b
+        while b != 0 {
+            (a, b) = (a ^ b, (a & b) << 1)
+        }
+        return a
+    }
+}
