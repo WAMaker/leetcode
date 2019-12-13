4c7b098bb7e731b49593fba0f614e158d9398b34
WAMaker <wamaker.scorpion@gmail.com>
2019-12-13 23:59:22 +0800

    commit solution

---
+++ Algorithms/252-Meeting-Rooms.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals = sorted(intervals, key=lambda x: (x[1], x[0]))
+        for i in range(1, len(intervals)):
+            if intervals[i - 1][1] > intervals[i][0]:
+                return False
+        return True
