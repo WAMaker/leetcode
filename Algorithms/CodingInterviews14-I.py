52de28c77eb78137b648581943a4baea26cea16c
WAMaker <wamaker.scorpion@gmail.com>
2020-03-07 23:24:06 +0800

    commit solution

---
+++ Algorithms/CodingInterviews14-I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def cuttingRope(self, n: int) -> int:
+        mp = {2: 1, 3: 2, 4: 4}
+        if n in mp:
+            return mp[n]
+        res = 1
+        while n:
+            if n in mp:
+                res *= n
+                n -= n
+            else:
+                res *= 3
+                n -= 3
+        return res
